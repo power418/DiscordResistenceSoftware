819695a593073789eef4a2ba7e821d68377e0ad5
power418 <neovecpp@gmail.com>
2026-05-08 13:40:52 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 // Pull in Windows UI baseline and manifest settings before module imports.
 #include <rpc/config/win32.h>
 
+#if defined(_WIN32) && defined(NDEBUG)
+#  include <windows.h>
+#endif
+
 import rpc;
 
 int main() {
   rpc::load_dotenv();
   return rpc::app::run();
 }
+
+#if defined(_WIN32) && defined(NDEBUG)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+  return main();
+}
+#endif
